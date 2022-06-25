aa54cdc5ea9ec8208f306575348bf387142bfa8e
Kennix Leung <kennixleung@Kennixs-MacBook-Pro.local>
2022-06-24 23:15:10 -0400

    initial commit

---
+++ .history/Server/Config/db_20220624230202.ts
@@ -0,0 +1,4 @@
+export const LocalURI = "mongodb://127.0.0.1/books229";
+export const RemoteURI = "mongodb+srv://kennixfun:YiElkAdWe8YVzM8M@cluster0.zjzj4y1.mongodb.net/?retryWrites=true&w=majority"
+export const HostName = "LocalHost";
+export const Secret = "someSecret";
---
+++ Server/Config/db.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Secret = exports.HostName = exports.RemoteURI = exports.LocalURI = void 0;
+exports.LocalURI = "mongodb://127.0.0.1/books229";
+exports.RemoteURI = "mongodb+srv://kennixfun:YiElkAdWe8YVzM8M@cluster0.zjzj4y1.mongodb.net/?retryWrites=true&w=majority";
+exports.HostName = "LocalHost";
+exports.Secret = "someSecret";
+//# sourceMappingURL=db.js.map
